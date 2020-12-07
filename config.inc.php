7f08f33536a7cf75575c91c3ad18cfd9deb931c6
nitinw1 <35346450+nitinw1@users.noreply.github.com>
2020-12-07 14:07:42 -0700

    Add files via upload

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'art');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+<?php 
+    define('DBHOST', 'localhost');
+    define('DBNAME', 'art');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME .";charset=utf8mb4;");
 ?>
