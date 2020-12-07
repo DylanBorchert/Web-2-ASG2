33d1bc1e0834d0f3f77920aa9ae7995c7ec94d64
CurtisMacLean <csjmaclean@gmail.com>
2020-12-07 15:38:47 -0700

    creating about

---
+++ about.php
@@ -0,0 +1,53 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>About</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital@1&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="./style/reset.css"> -->
+    <link rel="stylesheet" href="./style/index.css">
+    <link rel="stylesheet" href="./style/about.css">
+</head>
+<body>
+    <header id="header" class="nav">
+    <ul>
+        <li ><a href="index.php">Home</a></li>
+        <li ><a href="about.php">About</a></li>
+        <li ><a href="index.php">Galleries</a></li>
+        <li ><a href="index.php">Search</a></li>
+        <li ><a href="index.php">Favorites</a></li>
+    </ul>
+    </header>
+    <section id="header">
+        <h2>About Us</h2>
+    <p>This website is designed to take a collective of galleries which each contain a list of paintings and make the paintings accessible to the user.  
+        It is also capable of keeping favorites for a user using sessions.  
+        This website was designed for Randy Connolly’s COMP-3512 in the fall semester of 2020 at Mount Royal University. 
+    </p>
+    <ul>
+        <li>
+            <p>Dylan Borchert</p>
+            <a href="https://github.com/dborc610">GIT LInk</a>
+        </li>
+        <li>
+            <p>Curtis MacLean </p>
+            <a href="https://github.com/CurtisMacLean">GIT LInk</a>
+        </li>
+        <li>
+            <p>Justin Przybylski </p>
+            <a href="https://github.com/justprzy">GIT Link</a>
+        </li>
+        <li>
+            <p>Hudson Ukass </p>
+            <a href="https://github.com/hukas">GIT Link</a>
+        </li>
+        <li>
+            <p>Nitin Wadhwani </p>
+            <a href="https://github.com/nitinw1">GIT Link</a>
+        </li>
+    </ul>
+    </section>
+
+</body>
--- single-painting-tab.php
+++ single-painting-tab.php
@@ -24,7 +24,7 @@ if (isset($_GET['paintingid'])) {
 <html lang=en>
 
 <head>
-    <title>Lab 14</title>
+    <title>single painting</title>
     <meta charset="utf-8" />
     <title>Assignment 02</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
