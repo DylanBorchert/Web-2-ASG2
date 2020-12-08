bcc3f4b5776a5e0cbccd8215bc5fdb19dd50f856
CurtisMacLean <csjmaclean@gmail.com>
2020-12-07 19:10:34 -0700

    test

---
+++ browse-paintings.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <title>browse painting</title>
+    <meta charset="utf-8" />
+    <title>Assignment 02</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital@1&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="/asg2/Web-2-ASG-2/style/reset.css">  -->
+     <link rel="stylesheet" href="/asg2/Web-2-ASG2/style/index.css">
+     <link rel="stylesheet" href="/asg2/Web-2-ASG2/style/browse-paintings.css">
+    <script src="/asg2/Web-2-ASG2/single-painting-tab.js"></script>
+</head>
+
+<body>
+
+    <section id="header">
+        <h3>Browse Painting</h3>
+    </section>
+    <div id="filter">
+        <form>
+        <label for="title">Title:</label><br>
+        <input type="text" id="title" name="title" value=""><br>
+        </form>
+    </div>
+    <div id="view">
+    
+    </div>
+</body>
