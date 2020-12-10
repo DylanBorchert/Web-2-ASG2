5e940538b5b3186a649353e73f29bf3eb0bc9cd5
Hudson Ukass <31739503+hukas@users.noreply.github.com>
2020-12-09 23:47:05 -0700

    Update favorites.php

--- favorites.php
+++ favorites.php
@@ -40,8 +40,7 @@ $paintings = $paintingGate->getAll();
   }
   if (isset($_GET['paintingid-search'])) {
     addFavorite($_GET['paintingid-search']);
-    //header("Location: favorites.php?paintingid-search=" . $_GET['paintingid-search'] . "&title=" . $_GET['title'] . "&artist=" . $_GET['artist'] . "&museum=" . $_GET['museum']);
-    header("Location: favorites.php?title=" . $_GET['title'] . "&artist=" . $_GET['artist'] . "&museum=" . $_GET['museum']);
+    header("Location: browse-paintings.php?title=" . $_GET['title'] . "&artist=" . $_GET['artist'] . "&museum=" . $_GET['museum']);
   }
   //Array of paintings selected to be deleted.
   $selectedP = array();
